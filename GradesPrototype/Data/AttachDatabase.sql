ad5b4dfd6def23ed26ff3273fd220914649fbd83
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-11-06 15:56:22 +0100

    Tarea 4 Módulos 7 y 8-MOD07-LAK

---
+++ GradesPrototype/Data/AttachDatabase.sql
@@ -0,0 +1,8 @@
+﻿-- Run each line seperately and let it complete before running the next
+
+--sp_attach_db @dbname=[SchoolGradesDB], @filename1=[E:\Labfiles\Databases\Prototype\SchoolGradesDB.mdf]
+sp_attach_db @dbname=[SchoolDB]
+
+USE SchoolGradesDB;
+
+ALTER TABLE dbo.[Users] ADD [UserPassword] NVARCHAR (20) NOT NULL DEFAULT 'password'
